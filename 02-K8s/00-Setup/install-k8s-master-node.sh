12416a6c42b5c4d1b11b6142fec83a1732394827
root <root@master.example.com>
2020-11-09 11:39:12 +0000

    K8s-00-Setup

---
+++ 02-K8s/00-Setup/install-k8s-master-node.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "********************  Installing Docker ***********************"
+sleep 3
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+apt-get update && apt-get install -y docker-ce
+
+echo "******************** Installing kubernetes *********************"
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet=1.18.0-00 kubeadm=1.18.0-00 kubectl=1.18.0-00
+
+echo "******************* Deploying kubernetes ***********************"
+kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="10.128.0.3" 
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+
+echo "******************* Deploying kubernetes - Calico Network ***********************"
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
